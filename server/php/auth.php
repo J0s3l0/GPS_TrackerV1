0fea275f3225ea173dff1783b145a2cd1436dd78
Jose Luis <A01063645@itesm.mx>
2015-10-22 13:25:04 -0500

    add auth and config

---
+++ server/php/auth.php
@@ -0,0 +1,56 @@
+<?php
+    //load and connect to MySQL database stuff
+    require("config.php");
+    
+    $username       = isset($_POST['username']) ? $_POST['username'] : 0;
+    $password       = isset($_POST['password']) ? $_POST['password'] : 0;
+    
+    $query = "SELECT username, password, token FROM gps_users WHERE username = :username";
+    
+    $query_params = array(
+        ':username' => $username
+    );
+    
+    try {
+        $stmt   = $db->prepare($query);
+        $result = $stmt->execute($query_params);
+    }
+    catch (PDOException $ex) {
+        // For testing, you could use a die and message. 
+        //die("Failed to run query: " . $ex->getMessage());
+        
+        //or just use this use this one to product JSON data:
+        $response["success"] = 0;
+        $response["message"] = "Database Error1. Please Try Again!";
+        die(json_encode($response));
+        
+    }
+    
+    //This will be the variable to determine whether or not the user's information is correct.
+    //we initialize it as false.
+    $validated_info = false;
+    
+    //fetching all the rows from the query
+    $row = $stmt->fetch();
+    if ($row) {
+        //if we encrypted the password, we would unencrypt it here, but in our case we just
+        //compare the two passwords
+        if (md5($password) === $row['password']) {
+            $login_ok = true;
+        }
+    }
+    
+    // If the user logged in successfully, then we send them to the private members-only page 
+    // Otherwise, we display a login failed message and show the login form again 
+    if ($login_ok) {
+        $response["success"] = 1;
+        $response["token"] = $row['token'];
+        $response["message"] = "Login successful!";
+        die(json_encode($response));
+    } else {
+        $response["success"] = 0;
+        $response["message"] = "Invalid Credentials!";
+        die(json_encode($response));
+    }
+
+?>
